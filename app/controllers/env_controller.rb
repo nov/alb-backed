ba13cc8cb9785a6e6e57b8333a3658013660629d
Nov Matake <nov@matake.jp>
2018-06-06 13:53:08 +0900

    up

--- app/controllers/env_controller.rb
+++ app/controllers/env_controller.rb
@@ -1,7 +1,7 @@
 class EnvController < ApplicationController
   def show
     headers_hash = request.headers.inject({}) do |hash, (key, value)|
-      hash[key] = value if key ~= /^HTTP_/
+      hash[key] = value if key =~ /^HTTP_/
       hash.merge!(key => value)
     end
     render json: JSON.pretty_generate(headers_hash)
